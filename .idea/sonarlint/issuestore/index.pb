e722a534512d0af30ea19e8198d813a784faad9e
lauro <lauro@in8.com.br>
2023-09-13 11:09:45 +0100

    fist commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+k
+;app/src/main/java/com/hfad/secretmessage/MessageFragment.kt,d\7\d7cf0d407f6abf5c1b7e14a3e2208e92b7c5798b
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+k
+;app/src/main/java/com/hfad/secretmessage/WelcomeFragment.kt,6\f\6f24f1bab1932365d21cf067fbb6dffc3fddb29b
+h
+8app/src/main/java/com/hfad/secretmessage/MainActivity.kt,d\d\dd41fb5231bed599ae36a6435161fc266e3a1b73
---
+++ app/src/main/java/com/hfad/secretmessage/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.hfad.secretmessage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+class WelcomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startButton: Button = view.findViewById(R.id.start)
+        startButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_messageFragment)
+
+        }
+
+        return view
+    }
+
+}
