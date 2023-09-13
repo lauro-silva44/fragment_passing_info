794724c5861984d4c860642133bea3892eaae040
lauro <lauro@in8.com.br>
2023-09-13 19:52:49 +0100

    added the go back button

---
+++ app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
@@ -0,0 +1,26 @@
+package com.hfad.secretmessage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class EncryptFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val encryptView: TextView = view.findViewById(R.id.encrypt_message)
+
+        encryptView.text = message.reversed()
+        return view
+    }
+
+
+}
--- app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
 
@@ -12,9 +15,20 @@ class MessageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton: Button = view.findViewById(R.id.next)
+        val messageView: EditText =  view.findViewById(R.id.message)
+
+
+
+        nextButton.setOnClickListener {
+            val message =  messageView.text.toString()
+            val action = MessageFragmentDirections.actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        return view
     }
-
-
 }
